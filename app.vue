d211eff673bad05711df927a96547146c8264f3c
Kieran <kiermccarth+github@gmail.com>
2024-01-23 13:29:45 +0000

    UPDATE: Introduce initial fetch call for dataset

--- app.vue
+++ app.vue
@@ -3,3 +3,17 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import type { StatefulWordData } from "~/types/api.dataset";
+import type { UseFetchOptions } from "nuxt/app";
+import type { AsyncDataRequestStatus } from "nuxt/dist/app/composables/asyncData";
+
+const options: UseFetchOptions<StatefulWordData> = { deep: false };
+
+onBeforeMount(async () => {
+  const { data, status } = await useFetch("/api/dataset", options);
+  useState<StatefulWordData>("dataset", () => shallowRef(data));
+  useState<AsyncDataRequestStatus>("dataset-status", () => status);
+});
+</script>
