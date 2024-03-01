8d3f82eefb6caabafb50a4cbb8f14e5ba58f814f
Kieran <kiermccarth+github@gmail.com>
2024-03-01 16:28:05 +0000

    REFACTOR: Accessibility/maintainability pass
    Improve HTML semantics, improve custom element visual appearance to more closely mimic a native element, rename functions to better match use cases

--- components/AppSwitch.vue
+++ components/AppSwitch.vue
@@ -1,33 +1,56 @@
 <template>
-  <div class="switch" @click="toggleCheck()">
-    <label class="switch__label" :for="id">{{ label }}</label>
-    <input class="switch__input" type="checkbox" ref="input" :id="id" :name="id" />
-    <span class="switch__track"></span>
+  <div class="switch">
+    <label class="switch__label" :id="`${id}-label`" :for="id">{{ label }}</label>
+    <input
+      class="switch__input"
+      type="checkbox"
+      ref="input"
+      :id="id"
+      :name="id"
+      :aria-labelledby="`${id}-label`"
+      @input="handleCheck()"
+    />
+    <span class="switch__track" @click="handleClick()" aria-hidden="true"></span>
   </div>
 </template>
 
 <style scoped lang="scss">
-@use "../assets/styles/var/global";
+@use "../assets/styles/var/mixin";
 .switch {
-  --track-width: 3rem;
+  --track-width: var(--sz-xl);
   --track-height: calc(var(--track-width) / 2);
   --cursor-inset: calc(var(--track-width) / 12);
   --cursor-size: calc(var(--track-height) - calc(var(--cursor-inset) * 2));
 
+  all: unset;
+  isolation: isolate;
   display: grid;
   grid-template-columns: 1fr max-content;
-  column-gap: var(--sz-md);
+  column-gap: var(--sz-sm);
+  align-items: center;
+  padding: var(--sz-xs);
+  &:focus-within,
+  &:active {
+    .switch__track::after {
+      opacity: 1;
+    }
+  }
   &__input {
+    transition: outline 500ms var(--ef-out-quart);
     &[type="checkbox"] {
-      @include global.visuallyHidden();
+      @include mixin.visuallyHidden();
     }
     &:checked + .switch__track {
-      background-color: var(--cl-switch-active-background);
+      background-color: var(--cl-switch-background-active);
     }
     &:checked + .switch__track::before {
       --position: calc(100% - var(--cursor-size) - var(--cursor-inset));
 
       inset: 0 0 0 var(--position);
+      background-color: var(--cl-switch-handle-active);
+    }
+    &:focus {
+      outline: var(--sz-border-md) solid var(--cl-focus-highlight);
     }
   }
   &__track {
@@ -39,8 +62,9 @@
     width: var(--track-width);
     height: var(--track-height);
     border-radius: var(--outer-radius);
-    background-color: var(--cl-switch-inactive-background);
+    background-color: var(--cl-switch-background);
     transition: background-color 500ms var(--ef-out-quart);
+    // Switch handle
     &::before {
       content: "";
       position: absolute;
@@ -49,8 +73,20 @@
       width: var(--cursor-size);
       border-radius: var(--inner-radius);
       margin-block: auto;
-      background-color: var(--cl-text);
+      background-color: var(--cl-switch-handle);
       transition: inset 500ms var(--ef-out-quart);
+    }
+    // Generates focused border
+    &::after {
+      content: "";
+      position: absolute;
+      inset: 0;
+      border-radius: var(--outer-radius);
+      margin: calc(var(--sz-border-md) * -1);
+      z-index: -10;
+      background-color: var(--cl-focus-highlight);
+      opacity: 0;
+      transition: opacity 500ms var(--ef-out-quart);
     }
   }
 }
@@ -77,9 +113,16 @@ function emitValue(value: boolean) {
   emits("update:value", value);
 }
 
-function toggleCheck() {
+function handleCheck() {
   if (!input.value) return;
   const { checked } = input.value;
+  emitValue(checked);
+}
+
+function handleClick() {
+  if (!input.value) return;
+  const { checked } = input.value;
+  input.value.focus();
   emitValue(!checked);
 }
 </script>
