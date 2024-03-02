cebcb3a4c1cad7cb727433d6a86078befe662d5a
Kieran <kiermccarth+github@gmail.com>
2024-03-02 11:09:10 +0000

    ADD/MODIFY: Update CSS with accessibility pass and add shared variable for icon size

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,12 @@
 <template>
   <footer class="footer">
-    <a class="link" href="https://github.com/kier-mc" target="_blank">
-      <AppIcon class="icon" icon="github" />
+    <a
+      class="link"
+      href="https://github.com/kier-mc"
+      target="_blank"
+      aria-label="Visit Github (opens in new tab)"
+    >
+      <AppIcon class="icon" :size="iconSize" icon="github" title="Visit Github (opens in new tab)" />
     </a>
   </footer>
 </template>
@@ -16,8 +21,43 @@
   background-color: var(--cl-footer);
 }
 .link {
+  --size: v-bind(iconSize);
+
   all: unset;
+  display: flex;
   cursor: pointer;
+  position: relative;
+  aspect-ratio: 1/1;
+  border-radius: var(--size);
+  clip-path: circle(var(--size));
+  transition: outline 500ms var(--ef-out-quart);
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    aspect-ratio: 1/1;
+    clip-path: circle();
+    border-radius: var(--size);
+  }
+  &::before {
+    z-index: -5;
+    background-color: var(--cl-footer);
+    opacity: 1;
+  }
+  &::after {
+    margin: calc(var(--sz-border-lg) * -1);
+    z-index: -10;
+    background-color: var(--cl-focus-highlight);
+    opacity: 0;
+    transition: opacity 500ms var(--ef-out-quart);
+  }
+  &:focus {
+    outline: var(--sz-border-md) solid var(--cl-focus-highlight);
+    &::after {
+      opacity: 1;
+    }
+  }
 }
 .icon {
   fill: var(--cl-icon);
@@ -28,4 +68,8 @@
 }
 </style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IconSizes } from "~/utils/enums";
+
+const iconSize = ref<IconSizes>(IconSizes.md);
+</script>
