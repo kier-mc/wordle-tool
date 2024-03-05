c0efd9344f311f2af9614412c3396bb7a82e86ee
Kieran <kiermccarth+github@gmail.com>
2024-03-05 18:59:58 +0000

    ADD: Add TabData type

--- types/components.app.ts
+++ types/components.app.ts
@@ -1,3 +1,5 @@
 export type ValidColour = "base" | "brand" | "red" | "yellow" | "green";
 
 export type ValidInputStyle = "base" | "correct" | "valid" | "absent";
+
+export type TabData = { id: string; label: string };
