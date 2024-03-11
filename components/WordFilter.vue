80d921355440c88d5a8a6dfc101f8dbf279645d9
Kieran <kiermccarth+github@gmail.com>
2024-03-11 12:47:38 +0000

    ADD: Create filter component

---
+++ components/WordFilter.vue
@@ -0,0 +1,111 @@
+<template>
+  <article class="filters">
+    <h2>Filters</h2>
+    <AppTabs :tabs="tabs" v-model:active-tab="activeTab">
+      <template v-slot:boolean>
+        <FormSwitch
+          id="switch-potential-solutions"
+          label="Accepted Solutions Only"
+          :disabled="activeTab !== 'boolean'"
+          v-model:value="filters.flags.isPotentialSolution"
+        />
+        <FormSwitch
+          id="switch-potential-solutions"
+          label="Valid Input Only"
+          :disabled="activeTab !== 'boolean'"
+          v-model:value="filters.flags.isNotPotentialSolution"
+        />
+      </template>
+      <template v-slot:composition>
+        <FormSwitch
+          id="switch-duplicate-letters"
+          label="Duplicate Letter Words"
+          :disabled="flags.hasUniqueLetters || activeTab !== 'composition'"
+          v-model:value="flags.hasDuplicateLetters"
+        />
+        <FormSwitch
+          id="switch-unique-letters"
+          label="Unique Letter Words"
+          :disabled="flags.hasDuplicateLetters || activeTab !== 'composition'"
+          v-model:value="flags.hasUniqueLetters"
+        />
+      </template>
+    </AppTabs>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.filters {
+  display: grid;
+  row-gap: var(--sz-xs);
+}
+.filter {
+  display: grid;
+  row-gap: var(--sz-xs);
+  padding: var(--sz-xs);
+  background-color: var(--cl-footer);
+}
+</style>
+
+<script setup lang="ts">
+import type { WordData } from "~/types/api.dataset";
+import type { Filters, FilterConditions, FilterPredicate } from "~/types/components.word";
+
+const matches = useState<WordData[]>("matches");
+const localMatches = useState<WordData[]>("local-matches");
+
+const tabs = [
+  { id: "boolean", label: "Show" },
+  { id: "composition", label: "Word Composition" },
+];
+const activeTab = ref<string>(tabs[0].id);
+
+const filters = ref<Filters>({
+  flags: {
+    isPotentialSolution: false,
+    isNotPotentialSolution: false,
+    hasDuplicateLetters: false,
+    hasUniqueLetters: false,
+  },
+  composition: {
+    consonants: 0,
+    vowels: 0,
+  },
+});
+const { flags } = toRefs(filters.value);
+
+const conditions: FilterConditions = {
+  isPotentialSolution: (word) => word.metrics.is_potential_solution,
+  isNotPotentialSolution: (word) => !word.metrics.is_potential_solution,
+  hasDuplicateLetters: (word) => word.metrics.has_duplicate_letters,
+  hasUniqueLetters: (word) => !word.metrics.has_duplicate_letters,
+};
+
+function filterFlags(): FilterPredicate[] {
+  const predicates: ((word: WordData) => boolean)[] = [];
+
+  Object.entries(filters.value.flags).forEach(([filter, bool]) => {
+    if (bool) predicates.push(conditions[filter as keyof Filters["flags"]]);
+  });
+
+  return predicates;
+}
+
+function filterMatches(): WordData[] {
+  const payload = ref<WordData[]>(matches.value);
+  const predicates = ref<((word: WordData) => boolean)[]>([]);
+
+  predicates.value = [...filterFlags()];
+  const condition = (word: WordData) => predicates.value.every((predicate) => predicate(word));
+
+  if (!predicates.value.length) return matches.value;
+  return payload.value.filter(condition);
+}
+
+function syncLocalMatches(): void {
+  localMatches.value = filterMatches();
+}
+
+watch(filters, () => syncLocalMatches(), { deep: true });
+watch(matches, () => syncLocalMatches());
+</script>
