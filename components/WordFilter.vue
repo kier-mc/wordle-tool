f93b2c464f21d772bb87d09cca8d59977ec3e8c9
Kieran <kiermccarth+github@gmail.com>
2024-03-11 13:16:39 +0000

    REFACTOR: Convert template conditionals to state closure function

--- components/WordFilter.vue
+++ components/WordFilter.vue
@@ -2,17 +2,17 @@
   <article class="filters">
     <h2>Filters</h2>
     <AppTabs :tabs="tabs" v-model:active-tab="activeTab">
-      <template v-slot:boolean>
+      <template v-slot:flags>
         <FormSwitch
           id="switch-potential-solutions"
           label="Accepted Solutions Only"
-          :disabled="activeTab !== 'boolean'"
+          :disabled="disable.isPotentialSolution.value"
           v-model:value="filters.flags.isPotentialSolution"
         />
         <FormSwitch
           id="switch-potential-solutions"
           label="Valid Input Only"
-          :disabled="activeTab !== 'boolean'"
+          :disabled="disable.isNotPotentialSolution.value"
           v-model:value="filters.flags.isNotPotentialSolution"
         />
       </template>
@@ -20,13 +20,13 @@
         <FormSwitch
           id="switch-duplicate-letters"
           label="Duplicate Letter Words"
-          :disabled="flags.hasUniqueLetters || activeTab !== 'composition'"
+          :disabled="disable.hasDuplicateLetters.value"
           v-model:value="flags.hasDuplicateLetters"
         />
         <FormSwitch
           id="switch-unique-letters"
           label="Unique Letter Words"
-          :disabled="flags.hasDuplicateLetters || activeTab !== 'composition'"
+          :disabled="disable.hasUniqueLetters.value"
           v-model:value="flags.hasUniqueLetters"
         />
       </template>
@@ -55,10 +55,34 @@ const matches = useState<WordData[]>("matches");
 const localMatches = useState<WordData[]>("local-matches");
 
 const tabs = [
-  { id: "boolean", label: "Show" },
+  { id: "flags", label: "Solution" },
   { id: "composition", label: "Word Composition" },
 ];
 const activeTab = ref<string>(tabs[0].id);
+
+const disable = (function () {
+  const tab = computed(() => activeTab.value);
+
+  const isPotentialSolution = computed(() => {
+    return flags.value.isNotPotentialSolution || tab.value !== "flags";
+  });
+  const isNotPotentialSolution = computed(() => {
+    return flags.value.isPotentialSolution || tab.value !== "flags";
+  });
+  const hasDuplicateLetters = computed(() => {
+    return flags.value.hasUniqueLetters || tab.value !== "composition";
+  });
+  const hasUniqueLetters = computed(() => {
+    return flags.value.hasDuplicateLetters || tab.value !== "composition";
+  });
+
+  return {
+    isPotentialSolution,
+    isNotPotentialSolution,
+    hasDuplicateLetters,
+    hasUniqueLetters,
+  };
+})();
 
 const filters = ref<Filters>({
   flags: {
