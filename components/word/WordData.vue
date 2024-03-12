87f9ff64dab4676aa7b702346f4113ef05c6f777
Kieran <kiermccarth+github@gmail.com>
2024-03-12 10:55:56 +0000

    ADD: Basic WordData component

---
+++ components/word/WordData.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="word">
+    <AppButton class="result" colour="brand" @click="handleClick()">{{ word }}</AppButton>
+  </li>
+</template>
+
+<style scoped lang="scss">
+.word {
+  all: unset;
+}
+.result {
+  font-weight: 400;
+  width: 100%;
+}
+</style>
+
+<script setup lang="ts">
+import type { ValidWord } from "~/types/unions/api.dataset";
+
+const props = defineProps({
+  word: { type: String as PropType<ValidWord>, required: true },
+});
+
+const { word } = toRefs(props);
+
+const { getWordMetrics } = useDataset();
+
+function handleClick() {
+  const { term, metrics } = getWordMetrics(word)[0];
+  console.log(metrics);
+}
+</script>
