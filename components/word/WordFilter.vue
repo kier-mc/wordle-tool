6194e72d0a7ef17f9db19caca4fac9fc14d89596
Kieran <kiermccarth+github@gmail.com>
2024-03-14 13:47:44 +0000

    REFACTOR: Remove closure, replace with object returning computed values

--- components/word/WordFilter.vue
+++ components/word/WordFilter.vue
@@ -2,17 +2,17 @@
   <article class="filters">
     <h2>Filters</h2>
     <AppTabs :tabs="tabs" v-model:active-tab="activeTab">
-      <template v-slot:flags>
+      <template v-slot:solution>
         <FormSwitch
           id="switch-potential-solutions"
           label="Accepted Solutions Only"
-          :disabled="disable.isPotentialSolution.value"
+          :disabled="isDisabled.isPotentialSolution.value"
           v-model:value="flags.isPotentialSolution"
         />
         <FormSwitch
           id="switch-potential-solutions"
           label="Valid Input Only"
-          :disabled="disable.isNotPotentialSolution.value"
+          :disabled="isDisabled.isNotPotentialSolution.value"
           v-model:value="flags.isNotPotentialSolution"
         />
       </template>
@@ -20,13 +20,13 @@
         <FormSwitch
           id="switch-duplicate-letters"
           label="Duplicate Letter Words"
-          :disabled="disable.hasDuplicateLetters.value"
+          :disabled="isDisabled.hasDuplicateLetters.value"
           v-model:value="flags.hasDuplicateLetters"
         />
         <FormSwitch
           id="switch-unique-letters"
           label="Unique Letter Words"
-          :disabled="disable.hasUniqueLetters.value"
+          :disabled="isDisabled.hasUniqueLetters.value"
           v-model:value="flags.hasUniqueLetters"
         />
       </template>
@@ -52,32 +52,15 @@ const filters = useFilter().filters;
 const { flags } = toRefs(filters.value);
 
 const tabs = [
-  { id: "flags", label: "Solution" },
+  { id: "solution", label: "Solution" },
   { id: "composition", label: "Word Composition" },
 ];
 const activeTab = ref<string>(tabs[0].id);
 
-const disable = (function () {
-  const tab = computed(() => activeTab.value);
-
-  const isPotentialSolution = computed(() => {
-    return flags.value.isNotPotentialSolution || tab.value !== "flags";
-  });
-  const isNotPotentialSolution = computed(() => {
-    return flags.value.isPotentialSolution || tab.value !== "flags";
-  });
-  const hasDuplicateLetters = computed(() => {
-    return flags.value.hasUniqueLetters || tab.value !== "composition";
-  });
-  const hasUniqueLetters = computed(() => {
-    return flags.value.hasDuplicateLetters || tab.value !== "composition";
-  });
-
-  return {
-    isPotentialSolution,
-    isNotPotentialSolution,
-    hasDuplicateLetters,
-    hasUniqueLetters,
-  };
-})();
+const isDisabled = {
+  isPotentialSolution: computed(() => flags.value.isNotPotentialSolution || activeTab.value !== "solution"),
+  isNotPotentialSolution: computed(() => flags.value.isPotentialSolution || activeTab.value !== "solution"),
+  hasDuplicateLetters: computed(() => flags.value.hasUniqueLetters || activeTab.value !== "composition"),
+  hasUniqueLetters: computed(() => flags.value.hasDuplicateLetters || activeTab.value !== "composition"),
+};
 </script>
