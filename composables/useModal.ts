74a4bceb015e322f7c288ef79e390cd16ba32281
Kieran <kiermccarth+github@gmail.com>
2024-03-22 15:04:38 +0000

    MODIFY: Add JSDoc annotations

--- composables/useModal.ts
+++ composables/useModal.ts
@@ -1,12 +1,21 @@
+/**
+ * A composable that provides functions and properties associated with the app's modal component.
+ */
 export const useModal = function () {
   const state = useState<boolean>("modal-visible", () => false);
-
+  /**
+   * A computed property indicating the modal's visibility.
+   */
   const isVisible = computed(() => state.value);
-
+  /**
+   * Displays the app modal and any currently contained content.
+   */
   function show(): void {
     state.value = true;
   }
-
+  /**
+   * Hides the app modal and any currently contained content.
+   */
   function hide(): void {
     state.value = false;
   }
