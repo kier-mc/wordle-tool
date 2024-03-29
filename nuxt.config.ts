b5d6ebcd81c9407248204e8aa98f79b972f7a8b0
Kieran <kiermccarth+github@gmail.com>
2024-03-29 15:10:45 +0000

    MODIFY: Add VueUse to modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // app: {
-  //   head: {
-  //     link: [
-  //       {
-  //         rel: "stylesheet",
-  //         href: "https://fonts.googleapis.com/css?family=FONTS_TO_LOAD&display=swap",
-  //       },
-  //     ],
-  //   },
-  // },
+  app: {
+    head: {
+      // link: [
+      //   {
+      //     rel: "stylesheet",
+      //     href: "https://fonts.googleapis.com/css?family=FONTS_TO_LOAD&display=swap",
+      //   },
+      // ],
+      title: "Wordle Assistant",
+    },
+  },
   components: ["~/components", "~/components/icons"],
   css: ["~/assets/styles/index.scss"],
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  modules: ["@vueuse/nuxt"],
   nitro: {
     compressPublicAssets: true,
+    preset: "bun",
     serverAssets: [
       {
         baseName: "data",
