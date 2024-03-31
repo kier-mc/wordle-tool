3f4f6993cc8a01066aed42d4102ab9e72efb79f4
Kieran <kiermccarth+github@gmail.com>
2024-03-31 10:46:12 +0100

    ADD: Mount components and bridge logic for synchronising results

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
   <WordFinder />
+  <WordFilter />
+  <WordMatches />
 </template>
 
 <style scoped lang="scss"></style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { WordData } from "~/types/api.dataset";
+
+const matches = useState<WordData[]>("matches", () => shallowRef([]));
+const localMatches = useState<WordData[]>("local-matches", () => shallowRef([]));
+/**
+ * Synchronises core matches (directly from the finder component) with local matches (intended
+ * for filtering, sorting values etc. from the core matches).
+ */
+function syncLocalMatches(): void {
+  if (!matches.value.length) {
+    localMatches.value = [];
+    return;
+  }
+  localMatches.value = matches.value;
+}
+watch(matches, () => syncLocalMatches(), { immediate: true });
+</script>
