4ef6aa4865707abae9a1741a06c45d66a101f280
Kieran <kiermccarth+github@gmail.com>
2024-04-11 17:24:09 +0100

    ADD/REFACTOR: Integrate modal component, update template to reflect changes in WordData component and organise script

--- components/word/WordMatches.vue
+++ components/word/WordMatches.vue
@@ -3,13 +3,20 @@
     <section v-if="list.length" class="matches" aria-labelledby="heading-matches">
       <h2 id="heading-matches">Matches</h2>
       <ul class="matches__list" aria-labelledby="heading-matches">
-        <WordData v-for="word in list" :key="word.term" :word="word" />
+        <li v-for="word in list" :key="word.term" class="matches__match">
+          <WordData :word="word" @click="selectWord(word)" />
+        </li>
       </ul>
       <div class="matches__controls">
         <AppButton v-if="button.shouldDisplay()" @click="button.showMore()">Show More</AppButton>
       </div>
     </section>
   </Transition>
+  <Teleport to="body">
+    <AppModal :title="`Information for ${selectedWord?.term}`">
+      <WordMetrics :word="selectedWord" />
+    </AppModal>
+  </Teleport>
 </template>
 
 <style scoped lang="scss">
@@ -22,6 +29,9 @@
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(12ch, auto));
     gap: var(--sz-xs);
+  }
+  &__match {
+    all: unset;
   }
   &__controls {
     margin-top: calc(var(--sz-md) - var(--sz-sm));
@@ -44,12 +54,12 @@ import type { WordData } from "~/types/api.dataset";
 const matches = useState<WordData[]>("matches");
 const localMatches = useState<WordData[]>("local-matches");
 
-watch(matches, () => (pagination.value.index = 0));
-
 const pagination = ref({
   index: 0,
   size: 60 as const,
 });
+
+watch(matches, () => (pagination.value.index = 0));
 
 const list = computed(() => {
   const { index, size } = toRefs(pagination.value);
@@ -58,20 +68,44 @@ const list = computed(() => {
   return localMatches.value.slice(from, to);
 });
 
+const selectedWord = ref<WordData | null>(null);
+/**
+ * Sets the selectedWord reactive variable to the currently selected word and opens the modal.
+ * @param word
+ * A complete WordData object to be passed to the modal.
+ */
+function selectWord(word: WordData) {
+  const { show: showModal } = useModal();
+  selectedWord.value = word;
+  showModal();
+}
 const button = (function () {
   const { index, size } = toRefs(pagination.value);
+  /**
+   * Increments the pagination index by the defined size to display more results and scrolls to
+   * the bottom of the page.
+   */
   const showMore = function () {
     index.value += size.value;
     scrollToPageEnd();
   };
+  /**
+   * Determines if the paginated list length is less than the filtered matches, and therefore if
+   * the button to display more results should be displayed or not.
+   */
   const shouldDisplay = function () {
     return list.value.length < localMatches.value.length;
   };
 
   return { showMore, shouldDisplay };
 })();
-
-function scrollToPageEnd(): void {
-  setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 350);
+/**
+ * Helper function that scrolls to the bottom of the page after a given delay.
+ * @param delay
+ * The delay (in ms) to apply before scrolling.
+ * @default 350ms
+ */
+function scrollToPageEnd(delay: number = 350): void {
+  setTimeout(() => window.scrollTo(0, document.body.scrollHeight), delay);
 }
 </script>
