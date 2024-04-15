c0d891305cc11a8b84f4c88bab649b064118c73a
Kieran <kiermccarth+github@gmail.com>
2024-04-15 23:02:11 +0100

    MODIFY: Try import statement

--- server/api/dataset.ts
+++ server/api/dataset.ts
@@ -1,26 +1,6 @@
+import dataset from "~/public/assets/data/dataset.json";
 import type { WordData } from "~/types/api.dataset";
-import fs from "fs";
-import path from "path";
-
-function getDatasetPath() {
-  if (process.dev) return path.resolve("./public/assets/data", "dataset.json");
-  return "https://wordle-tool-git-dev-kier-mcs-projects.vercel.app/assets/data/dataset.json";
-}
 
 export default defineEventHandler(async (event) => {
-  const dataset = getDatasetPath();
-  console.log(`Using path: ${dataset}`);
-  try {
-    const response = await $fetch(dataset);
-    return response as WordData[];
-  } catch (error) {
-    console.error("Error fetching JSON file");
-    return {
-      status: 500,
-      body: { error: error },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-  }
+  return dataset as WordData[];
 });
