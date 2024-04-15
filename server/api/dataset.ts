250740dffc79bb32e46b5dd261ca9d52ac7a9b07
Kieran <kiermccarth+github@gmail.com>
2024-04-15 19:23:34 +0100

    REFACTOR: Serve dataset.json from public directory versus server

--- server/api/dataset.ts
+++ server/api/dataset.ts
@@ -1,8 +1,25 @@
 import type { WordData } from "~/types/api.dataset";
+import fs from "fs";
+import path from "path";
+
+function getDatasetPath() {
+  if (process.dev) return path.resolve("public/assets/data", "dataset.json");
+  return new URL("../public/assets/data/dataset.json", import.meta.url);
+}
 
 export default defineEventHandler(async (event) => {
-  const response = await useStorage<WordData[]>("assets:data").getItem(
-    "dataset.json"
-  );
-  return response;
+  const dataset = getDatasetPath();
+  try {
+    const data = fs.readFileSync(dataset, "utf-8");
+    return JSON.parse(data) as WordData[];
+  } catch (error) {
+    console.error("Error fetching JSON file");
+    return {
+      status: 500,
+      body: { error: error },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
 });
