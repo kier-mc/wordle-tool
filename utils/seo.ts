ce33421a02b88a68e9e64a62909e13ea0799f721
Kieran <kiermccarth+github@gmail.com>
2024-04-15 15:16:37 +0100

    ADD: Create SEO utils file

---
+++ utils/seo.ts
@@ -0,0 +1,9 @@
+import type { UseSeoMetaInput } from "@unhead/schema";
+
+// prettier-ignore
+export const seoMetadata: UseSeoMetaInput = {
+  title:          "Wordle Tool",
+  ogTitle:        "Wordle Tool",
+  description:    "Quickly find and filter through word matches for Wordle to improve your gameplay. Solve the puzzle outright, or simply get hints and assistance. Tailor everything to your needs.",
+  ogDescription:  "Quickly find and filter through word matches for Wordle to improve your gameplay. Solve the puzzle outright, or simply get hints and assistance. Tailor everything to your needs."
+};
